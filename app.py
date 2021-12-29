bedaedc21a764dcc3abf81542958b165feff7cc4
Marco <marconovino@gmail.com>
2021-12-29 20:44:24 -0300

    a

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ versionsDict = {
            }
 
 loop = asyncio.get_event_loop()
-versionList = asyncio.ensure_future(db.get_all_versions())
+versionList = asyncio.run(db.get_all_versions())
 
 for x in versionList:
     currID = x["versionid"]
