b7991be7921ab4b4eace3c0314688d637b17569a
Marco <marconovino@gmail.com>
2021-12-29 21:38:59 -0300

    a

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 versionsDict = {
-            {"0.0.1":"youtube.com"}, 
-            {"0.0.2":"you1tube.com"}
+            "0.0.1":"youtube.com", 
+            "0.0.2":"you1tube.com"
                }
 
 #cur.execute("SELECT * FROM Versions")
