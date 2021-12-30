7cba28115dc7525326033c359309668f32b44263
Marco <marconovino@gmail.com>
2021-12-29 21:41:13 -0300

    a

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_restful import Api, Resource
-#import psycopg2
-#import os
+import psycopg2
+import os
 
-#conn = psycopg2.connect(database=os.getenv('database'), user =os.getenv('user'), password = os.getenv('password'), host = os.getenv('host'), port = "5432")
-#print("Opened database successfully")
-#cur = conn.cursor()
+conn = psycopg2.connect(database=os.getenv('database'), user =os.getenv('user'), password = os.getenv('password'), host = os.getenv('host'), port = "5432")
+print("Opened database successfully")
+cur = conn.cursor()
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,14 +14,14 @@ versionsDict = {
             "0.0.2":"you1tube.com"
                }
 
-#cur.execute("SELECT * FROM Versions")
-#rows = cur.fetchall()
-#for row in rows:
-    #currID = row[0]
-    #currLink = row[1]
-    #versionsDict.update({currID:currLink})
-#print(versionsDict)
-#conn.close()
+cur.execute("SELECT * FROM Versions")
+rows = cur.fetchall()
+for row in rows:
+    currID = row[0]
+    currLink = row[1]
+    versionsDict.update({currID:currLink})
+print(versionsDict)
+conn.close()
 
 class Versions(Resource):
     def get(self, versionid):
