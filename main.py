fe844f9dc12749ae44e47abadd336ff486a11a68
Marco <marconovino@gmail.com>
2022-01-13 14:28:35 -0300

    aaaaaaaaa

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ async def on_raw_reaction_add(payload):
         if payload.emoji.name == '🥜':
             print("YT-Uploads")
             role = guild.get_role(883372754785681438)
-        member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+        member = payload.member
         print(role)
         print(member)
         if role is not None:
